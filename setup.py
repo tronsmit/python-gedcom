abe3d6a005928b2fff2c8d6fba9026ced52912bc
Nicklas Reincke <reincke@pier2port.de>
2018-01-09 10:54:50 +0100

    Updated `setup.py`

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 
 setup(
     name='python-gedcom',
-    version='0.1.1dev',
+    version='1.0.0dev',
     packages=['gedcom', ],
     license='GPLv2',
     package_dir={'': '.'},
-    description=open('README').readlines()[0].strip(),
-    long_description=open('README').read(),
-    maintainer='Madeleine Ball',
-    maintainer_email='mpball@gmail.com',
-    url='https://github.com/madprime/python-gedcom',
+    description=open('README.md').readlines()[0].strip(),
+    long_description=open('README.md').read(),
+    maintainer='Nicklas Reincke',
+    maintainer_email='contact@reynke.com',
+    url='https://github.com/reynke/python-gedcom',
 )
