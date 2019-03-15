c04b634521939382474f33337e81673fa6f6ff9a
Nicklas Reincke <contact@reynke.com>
2019-03-15 20:16:39 +0100

    Added test to parse a file

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,6 +1,27 @@
+from gedcom.element.individual import IndividualElement
 from gedcom.parser import Parser
 
 
 def test_initialization():
     parser = Parser()
     assert isinstance(parser, Parser)
+
+
+def test_parse_file():
+    parser = Parser()
+    parser.parse_file('tests/files/Musterstammbaum.ged')
+
+    individuals_in_root_child_elements = 0
+    individuals_in_element_list = 0
+
+    for element in parser.get_root_child_elements():
+        if isinstance(element, IndividualElement):
+            individuals_in_root_child_elements += 1
+
+    for element in parser.get_element_list():
+        if isinstance(element, IndividualElement):
+            individuals_in_element_list += 1
+
+    assert individuals_in_root_child_elements == 20
+    assert individuals_in_element_list == 20
+    assert len(parser.get_root_child_elements()) == 34
