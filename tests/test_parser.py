ea6a53f08b3fa772b4b9a807b79a7c56424b6f8d
Mohamed Ibrahim <mohamed_ameen@hotmail.com>
2020-02-21 05:20:34 -0800

    parse stream or lines array

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -49,6 +49,52 @@ def test_parse_file():
     assert individuals_in_root_child_elements == 20
     assert individuals_in_element_list == 20
 
+def test_parse_from_string():
+    case_1 = """0 @I5@ INDI
+1 NAME First /Last/
+1 SEX M
+1 BIRT
+2 DATE 1 JAN 1900
+2 PLAC Kirkland, King, Washington, USA
+3 MAP
+4 LATI N47.680663
+4 LONG W122.234319
+"""
+    gedcom_parser = Parser()
+    gedcom_parser.parse([(a+'\n').encode('utf-8-sig') for a in case_1.splitlines()])
+    element_1 = gedcom_parser.get_root_child_elements()[0]
+    assert isinstance(element_1, IndividualElement)
+    assert element_1.get_tag() == 'INDI'
+    assert element_1.get_pointer() == '@I5@'
+    element_1_children = element_1.get_child_elements()
+    assert len(element_1_children) == 3
+    assert element_1_children[0].get_tag() == 'NAME'
+    assert element_1_children[1].get_tag() == 'SEX'
+    assert element_1_children[2].get_tag() == 'BIRT'
+
+    case_2 = """0 @F28@ FAM
+1 HUSB @I80@
+1 WIFE @I81@
+1 CHIL @I9@
+2 _FREL Natural
+2 _MREL Natural
+1 CHIL @I84@
+2 _FREL Natural
+2 _MREL Natural
+1 CHIL @I85@
+2 _FREL Natural
+2 _MREL Natural
+"""
+    gedcom_parser.parse([(a+'\n').encode('utf-8-sig') for a in case_2.splitlines()])
+    element_2 = gedcom_parser.get_root_child_elements()[0]
+    assert element_2.get_tag() == 'FAM'
+    assert element_2.get_pointer() == '@F28@'
+    element_2_children = element_2.get_child_elements()
+    assert len(element_2_children) == 5
+    assert element_2_children[0].get_tag() == 'HUSB'
+    assert element_2_children[1].get_tag() == 'WIFE'
+    assert element_2_children[2].get_tag() == 'CHIL'
+    assert element_2_children[3].get_value() == '@I84@'
 
 def test___parse_line():
     # @TODO Add appropriate testing cases
